a38434242eddfcbd5f28fdbf5713fbd0d1d130fb
RosesAtlantis <lucass71102@gmail.com>
2024-03-11 17:10:42 -0300

    Jump Portotype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,23 +6,49 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump;
+
     private void Update()
     {
+        MovimentJump();
+        Movimentxy();
+    }
+
+    private void Movimentxy()
+    {
+
         if (Input.GetKey(KeyCode.A))
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(+speed, myRigidbody.velocity.y);
         }
 
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
     }
+
+    private void MovimentJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+          {
+            myRigidbody.velocity = Vector2.up * forceJump;
+          }
+    }   
 }
