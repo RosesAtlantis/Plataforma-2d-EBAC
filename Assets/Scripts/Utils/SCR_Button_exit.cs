3050b20672cf118357e81bcc7a08a06dfc51e69a
RosesAtlantis <lucass71102@gmail.com>
2024-03-11 11:08:11 -0300

    add menu and interate the game manager

---
+++ Assets/Scripts/Utils/SCR_Button_exit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Button_exit : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
